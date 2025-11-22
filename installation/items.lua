a1dc62aeacb382cce46a0a25c7e33e75b01f4c67
MIKAJYT <145557044+MIKAJYT@users.noreply.github.com>
2025-11-22 22:18:43 +0100

    Edited for jaksam_inventory

--- installation/items.lua
+++ installation/items.lua
@@ -4,59 +4,90 @@ Items = {
 
 -- Copy from HERE
 ['heavypc'] = {
-    label = 'Heavy Plate Carrier',
-    description = 'Modular vest with 2 plate slots.',
-    weight = 1500,
-    stack = false,
-    consume = 0,
-    client = {
-        export = 'next-kevlar.useVest'
-    },
-    buttons = {{
-        label = "Manage Plates",
-        action = function(slot)
-            exports['next-kevlar']:managePlates(slot)
+        label = 'Heavy Plate Carrier',
+        weight = 1.5,
+        stackable = false,
+        close = false,
+        description = 'Modular vest with 2 plate slots.',
+        maxStack = 100, -- does not actually stack because stackable is false
+        type = 'item',
+        contextActions = {
+    {
+        label = 'Manage Plates',
+        icon = 'bi-search',
+        callback = function(inventoryId, slotIndex)
+            exports['next-kevlar']:managePlates(slotIndex)
         end
-    }}
-},
+    }
+    },
+        oxClientExport = "next-kevlar.useVest"
+        
+    },
 
 ['lightpc'] = {
-    label = 'Light Plate Carrier',
-    description = 'Modular vest with a plate slot.',
-    weight = 1000,
-    stack = false,
-    consume = 0,
-    client = {
-        export = 'next-kevlar.useVest'
-    },
-    buttons = {{
-        label = "Manage Plates",
-        action = function(slot)
-            exports['next-kevlar']:managePlates(slot)
+        label = 'Light Plate Carrier',
+        weight = 1.0,
+        stackable = false,
+        close = false,
+        description = 'Modular vest with a plate slot.',
+        maxStack = 100, -- does not actually stack because stackable is false
+        type = 'item',
+    contextActions = {
+    {
+        label = 'Manage Plates',
+        icon = 'bi-search',
+        callback = function(inventoryId, slotIndex)
+            exports['next-kevlar']:managePlates(slotIndex)
         end
-    }}
-},
+    }
+    },
+    oxClientExport = "next-kevlar.useVest"
+        
+    },
 
 ['lightplate'] = {
-    label = 'Light Plate',
-    weight = 250,
-    stack = false,
-    description = 'A light plate, made of Polyethylene'
-},
+        label = 'Light Plate',
+        weight = 0.25,
+        stackable = false,
+        close = false,
+        description = 'A light plate, made of Polyethylene',
+        maxStack = 100,
+        type = 'item',
+        metadata = {
+            health = 25,
+            itemname = 'lightplate',
+        },
+        throwableOptions = false,
+    },
 
 ['heavyplate'] = {
-    label = 'Heavy Plate',
-    weight = 500,
-    stack = false,
-    description = 'A heavy plate, made of Ceramics'
-},
+        label = 'Heavy Plate',
+        weight = 0.5,
+        stackable = false,
+        close = false,
+        description = 'A heavy plate, made of Ceramics',
+        maxStack = 100, -- does not actually stack because stackable is false
+        type = 'item',
+        metadata = {
+            health = 50,
+            itemname = 'heavyplate',
+        },
+        throwableOptions = false,
+    },
 
 ['brokenplate'] = {
-    label = 'Broken Plate',
-    weight = 500,
-    stack = false,
-    description = 'This plate has shattered!'
-},
+        label = 'Broken Plate',
+        weight = 0.5,
+        stackable = false,
+        close = false,
+        description = 'This plate has shattered!',
+        maxStack = 100, -- does not actually stack because stackable is false
+        metadata = {
+            health = 0,
+            itemname = 'brokenplate',
+        },
+        throwableOptions = false,
+    },
 -- To HERE
 
 }
